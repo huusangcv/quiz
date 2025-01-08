194e9d80895b7079dbcc543c29c58647e0e491bd
huusangcv <huusangcv@gmail.com>
2025-01-08 16:36:46 +0700

    fix: update homepage github

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ export default function Loader() {
   return (
     <div className="loader-container">
       <div className="loader"></div>
-      <p>Loading questions...</p>
+      <p>Đang tải câu hỏi...</p>
     </div>
   );
 }
