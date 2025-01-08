91d0b6c9b797194a2fee9121a86c529afb4f50e4
huusangcv <huusangcv@gmail.com>
2025-01-08 18:00:38 +0700

    fix: update more website

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,14 @@ import ReactLogo from './React_logo1.png'; // Import the image
 function Header() {
   return (
     <header className="app-header">
-      <div style={{ marginTop: 10, fontSize: '1.6rem' }}>© coppy right to huusangcv</div>
+      <div style={{ marginTop: 10, fontSize: '1.6rem' }}>
+        Làm đuối quá thì giải trí ở đây đi{' '}
+        <h2>
+          <a href="https://newmoviesz.online/" target="_blank">
+            Xem phim
+          </a>
+        </h2>
+      </div>
       <h1 style={{ maxWidth: '100%', fontSize: '2.2rem' }}>Quản lý dự án phần mềm</h1>
     </header>
   );
